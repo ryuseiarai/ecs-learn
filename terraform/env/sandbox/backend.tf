f572dcd28aaed97c257df1a09956a667a44b22de
arai-ryusei <arai.rysuei.jp@gmail.com>
2022-03-30 00:14:06 +0900

    modify: git ignore

--- terraform/env/sandbox/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "common-s3-bucket-tfstate"
-    key            = "sandbox/terraform.tstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "common-dynamodb-table-tfstate"
-  }
-}
