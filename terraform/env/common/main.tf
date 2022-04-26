8abd275b65586fc3ecaecf6d00f8adad0a5ad6cb
arai-ryusei <arai.rysuei.jp@gmail.com>
2022-04-26 15:10:58 +0900

    Modify: tf_state module.

--- terraform/env/common/main.tf
+++ terraform/env/common/main.tf
@@ -3,7 +3,7 @@
 # Latest update: 2022/03/20
 
 module "tfstate" {
-  source            = "../../modules/tfstate"
+  source            = "../../modules/tf_state"
   bucket_name       = var.tfstate.bucket_name
   dynamo_table_name = var.tfstate.dynamo_table_name
   ip                = var.ip
---
+++ terraform/modules/tf_state/variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_name" {
+  default = {}
+}
+
+variable "dynamo_table_name" {
+  default = {}
+}
+
+variable "ip" {
+  default = {}
+}
